627a0bc7a75ea8103b99e108ecbde18f49634f58
JITENDRA MALAKALAPALLI <jitendra8911@gmail.com>
2023-03-09 19:24:22 -0600

    fixing routing

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,14 +3,16 @@ import { useLocation } from 'react-router-dom';
 import Card from '../Card/Card';
 import useDataFetcher from '../../custom-hooks/useDataFetcher';
 import * as dataConfig from '../../data/Metadata';
+import usePersonNameFromPath from '../../custom-hooks/usePersonNameFromPath';
 
 interface IProps {
   name: string;
 }
 function About(props: IProps) {
   const location = useLocation();
+  const personNameFromPath = usePersonNameFromPath();
   // @ts-ignore
-  const personName = location.state ? location.state.personName : props.name;
+  const personName = personNameFromPath || (location.state ? location.state.personName : props.name);
   const markDownFilePath = `${dataConfig.aboutDataPath}/${personName}/${personName}.md`;
   const imagePath = `${dataConfig.aboutImagesPath}/${personName}.JPG`;
   const [md, imageFile] = useDataFetcher(imagePath, markDownFilePath);
--- src/custom-hooks/index.ts
+++ src/custom-hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useOnClickOutside } from './useOnClickOutside';
 export { default as useDataFetcher } from './useDataFetcher';
+export { default as usePersonNameFromPath } from './usePersonNameFromPath';
