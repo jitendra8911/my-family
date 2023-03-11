3a9073d284c531f2f03039011f3d93c03248cf4b
JITENDRA MALAKALAPALLI <jitendra8911@gmail.com>
2023-03-11 08:00:23 -0600

    fix incompatible regex for ios and mac os

--- src/custom-hooks/usePersonNameFromPath.tsx
+++ src/custom-hooks/usePersonNameFromPath.tsx
@@ -2,11 +2,12 @@ import { useLocation } from 'react-router-dom';
 
 function usePersonNameFromPath() {
   const location = useLocation();
-  const personNameRegex = /(?<=(moments|about)\/)\w+/g;
+  const personNameRegex = /((moments|about)\/)\w+/g;
 
-  const matchedPersonNameFromRegex = location.pathname.match(personNameRegex) || [];
+  const matchedPersonNameResults = location.pathname.match(personNameRegex) ?? [];
+  const matchedExpression = matchedPersonNameResults[0];
 
-  return matchedPersonNameFromRegex[0];
+  return matchedExpression && matchedExpression.split('/')[1];
 }
 
 export default usePersonNameFromPath;
